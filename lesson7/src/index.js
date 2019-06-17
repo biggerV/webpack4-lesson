9bf8ec5ed748213d0c2ede3c4eab7054a76448c5
黄俊辉 <huangjunhui19@crc.com.hk>
2019-06-17 11:03:27 +0800

    lesson 7 : 模块热更新

---
+++ lesson7/src/index.js
@@ -0,0 +1,22 @@
+import './styles/style.css'
+import avaterUrl from '@/assets/avatar.jpg'
+
+var a = "hot module replacement! 999"
+function b() {
+  document.querySelector("#app").innerHTML = a
+  document.querySelector("#app").classList = "red"
+}
+b()
+
+var avatar = new Image()
+avatar.src = avaterUrl
+document.querySelector("body").appendChild(avatar)
+
+console.warn('hot module replacement! 1')
+
+// 必须加上这段，否则会刷新页面
+// 如果用了其他loader，这个是不用加的，比如vue-loader
+// loader会自动给你加上
+if (module.hot) {
+  module.hot.accept()
+}
