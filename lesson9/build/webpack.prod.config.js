b05205d63997be07aa9d9dd3b6b60034c6b6d3d5
黄俊辉 <huangjunhui19@crc.com.hk>
2019-06-20 14:59:08 +0800

    修复process.env.NODE_ENV设置

--- lesson9/build/webpack.prod.config.js
+++ lesson9/build/webpack.prod.config.js
@@ -12,7 +12,10 @@ module.exports = webpackMerge(webpackCommonConfig, {
       // 这让我们可以只在common配置里面配置css相关loader，并只在dev环境有sourceMap，
       // 简化了我们的配置
       // 事实上所有插件都会检查这个参数，如果是production将会生成适用于生产环境的代码，精简是其中之一特点
-      'process.env.NODE_ENV': "production"
+
+      //注意，因为这个插件直接执行文本替换，给定的值必须包含字符串本身内的实际引号。
+      // 通常，有两种方式来达到这个效果，使用 '"production"', 或者使用 JSON.stringify('production')。
+      'process.env.NODE_ENV': JSON.stringify('production')
     })
   ]
 })
--- lesson9/src/index.js
+++ lesson9/src/index.js
@@ -21,6 +21,8 @@ document.querySelector("body").appendChild(avatar)
 
 console.warn('hot module replacement! 1')
 
+alert(process.env.NODE_ENV)
+
 // 必须加上这段，否则会刷新页面
 // 如果用了其他loader，这个是不用加的，比如vue-loader
 // loader会自动给你加上
