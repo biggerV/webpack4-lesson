05ec314e8e5b65befe92e8d170a52055475e3902
黄俊辉 <huangjunhui19@crc.com.hk>
2019-07-23 10:53:38 +0800

    pathRewrite

--- lesson16/build/webpack.dev.config.js
+++ lesson16/build/webpack.dev.config.js
@@ -26,7 +26,8 @@ module.exports = webpackMerge(webpackCommonConfig, {
       '/v2': {
         target: 'https://api.douban.com', // 将本地http://localhost地址代理到此地址上
         changeOrigin: true, // 改变请求主机地址，设置为true变为target的地址，false则为localhost，一般设置为true，否则服务器可能会拒绝访问
-        secure: false // https下如果证书无效时，需要设置为false，否则devServer会拒绝接收，但是如果证书有效的话设置true/false都可以
+        secure: false, // https下如果证书无效时，需要设置为false，否则devServer会拒绝接收，但是如果证书有效的话设置true/false都可以
+        // pathRewrite: {"^/v2": ""} // 地址重写 如果API地址和本地地址有所不同，可以通过这个替换地址中的匹配项
       }
     }
   }
